6e567502962075610e0cbddcdc9e4d89d479fefb
Kamran Babazadeh <99259950+CamCodesSwiftly@users.noreply.github.com>
2024-03-05 00:57:34 +0100

    feat: Receive input and handle some conversions
    Also adjusted the class to my needs and a bit more sense.
    Siwtched it to a getter setter structure with own variables.
    Whoever wrote it with only getters, I did not understand at all.

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,80 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
-  };
+	this.initNum = 0;
+	this.initUnit = "";
+	this.returnNum = 0;
+	this.returnUnit = "";
 
-  this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
-  };
-  
-  this.convert = function(initNum, initUnit) {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
-    let result;
-    
-    return result;
-  };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
-  };
-  
+	this.setNum = function (input) {
+		this.initNum = Number(input);
+	};
+
+	this.setUnit = function (input) {
+		this.initUnit = input;
+	};
+
+	this.getNum = function () {
+		return this.initNum;
+	};
+
+	this.getUnit = function () {
+		return this.initUnit;
+	};
+
+	this.getReturnUnit = function (initUnit) {
+		const switchUnit = this.initUnit ? this.initUnit : initUnit;
+
+		switch (switchUnit) {
+			case "gal":
+				return "L"; // Gallons to Liters
+			case "L":
+				return "gal"; // Liters to Gallons
+			case "mi":
+				return "km"; // Miles to Kilometers
+			case "km":
+				return "mi"; // Kilometers to Miles
+			case "lbs":
+				return "kg"; // Pounds to Kilograms
+			case "kg":
+				return "lbs"; // Kilograms to Pounds
+			default:
+				return null; // Handle unsupported units or return an error
+		}
+	};
+
+	//TODO ALL
+	this.spellOutUnit = function (unit) {
+		switch (unit) {
+			case "gal":
+				return "gallons";
+			case "L":
+				return "liters";
+			case "mi":
+				return "miles";
+			case "km":
+				return "kilometers";
+			case "lbs":
+				return "pounds";
+			case "kg":
+				return "kilograms";
+			default:
+				return null; // Handle unsupported units or return an error
+		}
+	};
+
+	this.convert = function (initNum, initUnit) {
+		const galToL = 3.78541;
+		const lbsToKg = 0.453592;
+		const miToKm = 1.60934;
+		let result;
+
+		return result;
+	};
+
+	this.getString = function (initNum, initUnit, returnNum, returnUnit) {
+		let result;
+
+		return result;
+	};
 }
 
 module.exports = ConvertHandler;
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,53 @@
-'use strict';
+"use strict";
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
-  
-  let convertHandler = new ConvertHandler();
+	let convertHandler = new ConvertHandler();
+	app.get("/api/convert", (req, res) => {
+		// console.log(req.query.input);
 
+		const { numbersString, charactersString } = splitInputString(
+			req.query.input
+		);
+
+		console.log("Numbers:", numbersString);
+		console.log("Characters:", charactersString);
+
+		convertHandler.setNum(numbersString);
+		convertHandler.setUnit(charactersString);
+
+		console.log("initNum", convertHandler.getNum());
+		console.log("initUnit", convertHandler.getUnit());
+		console.log("returnUnit", convertHandler.getReturnUnit());
+		console.log(
+			"spellunit",
+			convertHandler.spellOutUnit(convertHandler.getUnit())
+		);
+
+		res.json({
+			initNum: 0,
+			initUnit: "cm",
+			returnNum: 1,
+			returnUnit: "ft",
+			string: `Der Retörn string`,
+		});
+	});
 };
+
+// TODO: Splitstring function
+function splitInputString(inputString) {
+	const nonNumericIndex = Array.from(inputString).findIndex((char) =>
+		isNaN(Number(char))
+	);
+
+	// Extract numeric and non-numeric parts
+	const numbersString = inputString.substring(0, nonNumericIndex);
+	const charactersString = inputString.substring(nonNumericIndex);
+
+	return {
+		numbersString: numbersString,
+		charactersString: charactersString,
+	};
+}
