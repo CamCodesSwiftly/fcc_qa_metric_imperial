f382a50ab95bed68aa11490f285d66b0f26c99d5
Kamran Babazadeh <99259950+CamCodesSwiftly@users.noreply.github.com>
2024-03-06 21:37:05 +0100

    feat: Solve all: Add all functional tests and pass them.
    Learned a lot here again. Specifically, how to make GET requests with chai and mocha.
    The syntax is pleasingly easy. Better than what CHATGPT had to offer to me which i understood was only chai.
    Thank you freecodecamp. You guys are fucking incredible and I will be forever thankful.
    When I ever will reach a point where I earn significant money, I will not forget you guys.
    Money will come.

--- routes/api.js
+++ routes/api.js
@@ -6,24 +6,16 @@ const ConvertHandler = require("../controllers/convertHandler.js");
 module.exports = function (app) {
 	let convertHandler = new ConvertHandler();
 	app.get("/api/convert", (req, res) => {
-		console.log("reqqueryinput: " + req.query.input);
 		const inputNum = convertHandler.getNum(req.query.input);
-		console.log("inputNum: " + inputNum);
 		const inputUnit = convertHandler.getUnit(req.query.input);
-		console.log("inputUnit: " + inputUnit);
 		const returnUnit = convertHandler.getReturnUnit(inputUnit);
-		console.log("returnUnit: " + returnUnit);
 		const returnNum = convertHandler.convert(inputNum, inputUnit);
-		console.log("returnNum: " + returnNum);
 		const returnString = convertHandler.getString(
 			inputNum,
 			inputUnit,
 			returnNum,
 			returnUnit
 		);
-		console.log("returnString: " + returnString);
-		console.log("-------------------------------------------");
-		console.log(" ");
 
 		// return invalid if necessary
 		if (inputNum === "invalid number" && returnUnit === "invalid unit") {
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,102 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 let assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-
+suite("Functional Tests", function () {
+	//#1
+	test("#Convert 10L correct via GET /api/convert", () => {
+		chai.request(server)
+			.get("/api/convert")
+			.query({ input: "10L" }) // Provide the desired query parameters
+			.end(function (err, res) {
+				assert.equal(err, null); // No error should occur
+				assert.equal(res.status, 200);
+				assert.property(res.body, "initNum", "initNum does not exist");
+				assert.property(
+					res.body,
+					"initUnit",
+					"initUnit does not exist"
+				);
+				assert.property(
+					res.body,
+					"returnNum",
+					"returnNum does not exist"
+				);
+				assert.property(
+					res.body,
+					"returnUnit",
+					"returnUnit does not exist"
+				);
+				assert.property(res.body, "string", "string does not exist");
+				// Add specific expectations for returnNum and returnUnit
+				assert.equal(res.body.returnNum, 2.64172);
+				assert.equal(res.body.returnUnit, "gal");
+			});
+	});
+	//#2
+	test("#Convert 32g correct via GET /api/convert", () => {
+		chai.request(server)
+			.get("/api/convert")
+			.query({ input: "32g" }) // Provide the desired query parameters
+			.end(function (err, res) {
+				assert.equal(err, null); // No error should occur
+				assert.equal(res.status, 200);
+				assert.equal(res.text, "invalid unit");
+			});
+	});
+	//#3
+	test("#Convert 3/7.2/4kg correct via GET /api/convert", () => {
+		chai.request(server)
+			.get("/api/convert")
+			.query({ input: "3/7.2/4kg" }) // Provide the desired query parameters
+			.end(function (err, res) {
+				assert.equal(err, null); // No error should occur
+				assert.equal(res.status, 200);
+				assert.equal(res.text, "invalid number");
+			});
+	});
+	//#4
+	test("#Convert 3/7.2/4kilomegagram correct via GET /api/convert", () => {
+		chai.request(server)
+			.get("/api/convert")
+			.query({ input: "3/7.2/4kilomegagram" }) // Provide the desired query parameters
+			.end(function (err, res) {
+				assert.equal(err, null); // No error should occur
+				assert.equal(res.status, 200);
+				assert.equal(res.text, "invalid number and unit");
+			});
+	});
+	//#5
+	test("#Convert kg correct via GET /api/convert", () => {
+		chai.request(server)
+			.get("/api/convert")
+			.query({ input: "kg" }) // Provide the desired query parameters
+			.end(function (err, res) {
+				assert.equal(err, null); // No error should occur
+				assert.equal(res.status, 200);
+				assert.property(res.body, "initNum", "initNum does not exist");
+				assert.property(
+					res.body,
+					"initUnit",
+					"initUnit does not exist"
+				);
+				assert.property(
+					res.body,
+					"returnNum",
+					"returnNum does not exist"
+				);
+				assert.property(
+					res.body,
+					"returnUnit",
+					"returnUnit does not exist"
+				);
+				assert.property(res.body, "string", "string does not exist");
+				// Add specific expectations for returnNum and returnUnit
+				assert.equal(res.body.returnNum, 2.20462);
+				assert.equal(res.body.returnUnit, "lbs");
+			});
+	});
 });
